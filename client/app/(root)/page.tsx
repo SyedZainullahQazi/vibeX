1123d0ea823bfef7e2bce6196aff138a58c97f53
Haroonali787 <101257457+Haroonali787@users.noreply.github.com>
2023-12-05 02:46:57 +0500

    Thread Card functionality/fetching

--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -30,12 +30,12 @@ export default async function Home() {
             <ThreadCard
               key={post._id}
               id={post._id}
-              currentUserId={user?.id}
+              currentUserId={user?.id || ""}
               parentId={post.parentId}
               content={post.text}
               author={post.author}
               community={post.community}
-              created={post.createdAt}
+              createdAt={post.createdAt}
               comments={post.children}
             />
           ))}
