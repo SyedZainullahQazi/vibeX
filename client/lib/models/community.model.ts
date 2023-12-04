fd87109ca9e7bafa87aa8d0b6321f15181538e67
Ali Hussain <alihussainch633@gmail.com>
2023-12-05 00:52:35 +0500

    community feature

---
+++ client/lib/models/community.model.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  createdBy: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  //multiple user can be members of specific communities 
+  members: [
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+    }  
+  ]
+
+});
+
+const Community = mongoose.models.Community || mongoose.model("Community", communitySchema);
+
+export default Community;
