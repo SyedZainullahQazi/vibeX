e147a1c2a89995f3cf865f398f1071ac1bb4f8ea
syedzainullahqazi <f200328@cfd.nu.edu.pk>
2023-12-12 01:53:14 +0500

    bug changes

--- client/app/(auth)/onboarding/page.tsx
+++ client/app/(auth)/onboarding/page.tsx
@@ -1,38 +1,37 @@
-import AccountProfile from "@/components/forms/AccountProfile"
-import { currentUser } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
 
-async function Page () {
-  // to get the user data from the clerk 
-  const user = await currentUser()
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-  // it will fetch the data of the users (not the current user) from the database
-  const userInfo = {}
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
-  // object of the user data 
   const userData = {
-
-    id: user?.id, // id of the currently logged in user from clerk
-    objectID: userInfo?._id, // ? is the "if exists"
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user?.imageUrl,
-
-  }
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
 
   return (
-    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
-      <p className="mt-3 text-base-regular text-light-2">
-        Complete your profile now to use vibeX
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now, to use Threds.
       </p>
 
-      <section className="mt-9 bg-dark-2 p-10">
-        <AccountProfile user={userData} btnTitle="Continue"/>
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
       </section>
     </main>
-  )
+  );
 }
 
-export default Page
+export default Page;
--- client/components/forms/PostThread.tsx
+++ client/components/forms/PostThread.tsx
@@ -18,7 +18,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter } from 'next/navigation'
 import { ThreadValidation } from '@/lib/validations/threads';
 import { createThread } from '@/lib/actions/thread.actions';
-
+import { useOrganization } from '@clerk/nextjs';
 // import { updateUser } from "@/lib/actions/user.actions";
 
 // defining Props as interface (whichh is giving its structure)
@@ -36,8 +36,10 @@ interface Props {
 
 
 function PostThread({ userId }: { userId: string }) {
+
     const router = useRouter()
     const pathname = usePathname()
+    const { organization } = useOrganization();
 
     // we have to declare the form data that we have to push into it
 
@@ -53,10 +55,11 @@ function PostThread({ userId }: { userId: string }) {
     });
 
     const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
-        await createThread({
-            text: values.thread, author: userId, communityId: null, path: pathname
-        })
 
+            await createThread({
+                text: values.thread, author: userId, communityId:organization?organization.id:null, 
+                path: pathname
+            })
         router.push('/')
     }
 
