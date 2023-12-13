2d654c9e107e7070143ac293ee82120e214d68d2
syedzainullahqazi <f200328@cfd.nu.edu.pk>
2023-12-13 05:48:00 +0500

    center auth/validation60chars

--- client/lib/validations/threads.ts
+++ client/lib/validations/threads.ts
@@ -4,7 +4,7 @@ import * as z from 'zod'
 export const ThreadValidation = z.object({
     // defining different fields 
     // this ensurs that profile url is always an string 
-    thread: z.string().min(3, { message: "MINIMUM 3 CHARACTERS" }).max(30, { message: "MAXIMUM 30 CHARACTERS" }),
+    thread: z.string().min(3, { message: "MINIMUM 3 CHARACTERS" }).max(60, { message: "MAXIMUM 60 CHARACTERS" }),
     accountId: z.string()
 })
 
@@ -12,5 +12,5 @@ export const ThreadValidation = z.object({
 export const CommentValidation = z.object({
     // defining different fields 
     // this ensurs that profile url is always an string 
-    thread: z.string().min(3, { message: "MINIMUM 3 CHARACTERS" }).max(30, { message: "MAXIMUM 30 CHARACTERS" }),
+    thread: z.string().min(3, { message: "MINIMUM 3 CHARACTERS" }).max(60, { message: "MAXIMUM 60 CHARACTERS" }),
 })
